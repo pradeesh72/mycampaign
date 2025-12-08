7fd36814456e7ad5b9b1a8830622c5b66ad35721
RASNA <rasna@apples-MacBook-Pro.local>
2025-12-08 18:51:51 +0530

    Fix beep issue

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
 const clickDiv = document.getElementById("myVote");
 const parentDiv = clickDiv.closest(".arrow-left");
 const modal = new bootstrap.Modal(document.getElementById("myModal"));
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+const oscillator = audioCtx.createOscillator();
+oscillator.type = "squre";
+oscillator.frequency.setValueAtTime(800, audioCtx.currentTime); 
+oscillator.connect(audioCtx.destination);
 const buttonClick = (e) => {
     e.preventDefault();
     parentDiv.classList.add('active');
-    const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-  const oscillator = audioCtx.createOscillator();
-  oscillator.type = "squre";
-  oscillator.frequency.setValueAtTime(800, audioCtx.currentTime); 
-  oscillator.connect(audioCtx.destination);
   oscillator.start();
   oscillator.stop(audioCtx.currentTime + 1); 
   setTimeout(() => {
